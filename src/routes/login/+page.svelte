c8f6e1b6282630302e6c38e7f0fe21e6d0821aa9
Pontus <pontus.z94@gmail.com>
2023-11-04 14:12:42 +0100

    basic llogin and register form + logic

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,171 @@
+<script>
+  import { goto } from "$app/navigation"
+
+  let email = "pontus@zetterberg.io"
+  let password = "qew123w2"
+  let message = ""
+
+  async function handleLogin() {
+    const credentials = { email, password }
+
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        goto("/new-url")
+      } else {
+      }
+    } catch (error) {}
+  }
+</script>
+
+<main>
+  <div class="form__container">
+    <form>
+      <h1>Sign in</h1>
+      <div class="form__item">
+        <label for="email">Email</label>
+        <input
+          class="form__input"
+          required
+          type="email"
+          id="email"
+          bind:value={email}
+        />
+      </div>
+
+      <div class="form__item">
+        <label for="password">Password:</label>
+        <input
+          class="form__input"
+          required
+          type="password"
+          id="password"
+          bind:value={password}
+        />
+        <a href="#">Forgot password?</a>
+      </div>
+
+      <button class="submit" on:submit={handleLogin}>Sign in</button>
+    </form>
+    <span class="login__redirect"
+      >Don't have an account? <a href="#">Sign up here</a></span
+    >
+  </div>
+</main>
+
+<style lang="scss">
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    font-family: "Poppins", sans-serif;
+    background: #f5f6f8;
+  }
+  * {
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+
+    form {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      max-width: 400px;
+      gap: 1em;
+    }
+  }
+  .form__container {
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 5px 10px;
+    width: 400px;
+    height: 500px;
+    background-color: #fcfcfc;
+    padding: 1em;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+  }
+  h1 {
+    margin-bottom: 0.5em;
+  }
+  .form__item {
+    display: flex;
+    flex-direction: column;
+
+    a {
+      text-decoration: none;
+      margin-top: 0.2em;
+      color: rgba(22, 22, 22, 0.699);
+    }
+  }
+  .form__input {
+    box-sizing: border-box;
+    border: 1px solid;
+    border-radius: 0px;
+    border-color: lightgrey;
+    background: #ffffff;
+    height: 45px;
+    padding: 5px;
+    transition: border-color ease-in 150ms;
+    border-radius: var(--border-radius);
+    font-size: 14px;
+  }
+  label {
+    font-weight: 600;
+    align-self: start;
+    margin-bottom: 0.2em;
+  }
+  input:focus {
+    border: 1px solid rgba(0, 0, 0, 0.5);
+    outline: transparent;
+  }
+  .submit {
+    all: unset;
+    width: 100%;
+    color: var(--primary-button-text, black);
+    background-color: var(--primary-button, #c2ebd1);
+    height: 45px;
+    border-radius: var(--border-radius);
+    text-transform: uppercase;
+    letter-spacing: 0.2em;
+    font-weight: 600;
+    cursor: pointer;
+    transition: background-color ease-in 150ms, opacity ease-in 150ms;
+    text-align: center;
+    min-height: 45px;
+    transition: scale ease-in 150ms;
+    &:hover {
+      background-color: var(--primary-button, #c2ebd1);
+      opacity: 0.85;
+    }
+    &:disabled {
+      background-color: var(--primary-button, #c2ebd1);
+      opacity: 0.3;
+      cursor: not-allowed;
+    }
+    &:active:not(:disabled) {
+      scale: 1.02;
+    }
+  }
+  .login__redirect {
+    text-align: center;
+
+    a {
+      opacity: 1;
+      color: rgb(22, 22, 22);
+      font-weight: 600;
+    }
+  }
+</style>
